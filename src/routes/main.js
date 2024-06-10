bdc197290da968665951e65ba872cf98ee21ea4a
RodrigoGraciotto <rodrigograciotto@hotmail.com>
2024-06-10 11:05:55 -0300

    first commit

---
+++ src/routes/main.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { prisma } from "../../config/prisma.js";
+
+const router = Router()
+
+router.get("/ping", (req, res) => {
+    res.json({pong : true})
+})
+
+export default router
